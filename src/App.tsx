9f9277b077624dc68fa8209864803938db3799b8
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-07-19 11:08:25 -0300

    Enhance styling and structure: Update index.html with a background class, add Text component for consistent typography, and include class-variance-authority for variant management in styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import Text from "./components/text";
+
 export default function App() {
-  return <div className="">ToDo List</div>;
+  return (
+    <div className="flex flex-col gap-4">
+      <Text variant="body-sm-bold" className="text-pink-base">
+        ToDo List
+      </Text>
+      <Text className="text-green-base">ToDo List</Text>
+      <Text variant="body-md-bold">ToDo List</Text>
+    </div>
+  );
 }
