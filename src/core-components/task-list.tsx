001e8dae3ca7af3ea2c5d1a41add52ac58d62354
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-09-08 13:33:15 -0300

    feat(tasks): Adds a state manager for task creation. Creates the `use-task.ts` hook to centralize task handling logic, using `use-local-storage` for data persistence. The `prepareTask` function was implemented to add a new task with an initial state of 'Creating,' allowing for future implementation of in-place editing. The `task-list.tsx` component was modified to use this new hook, connecting the "New Task" button action to the `prepareTask` function, decoupling the state logic from the UI component.

--- src/core-components/task-list.tsx
+++ src/core-components/task-list.tsx
@@ -2,16 +2,21 @@ import Button from "../components/button";
 import PlusIcon from "../assets/icons/plus.svg?react";
 import TaskItem from "../core-components/task-item";
 import useTasks from "../hooks/use-tasks";
+import useTask from "../hooks/use-task";
 
 export default function TaskList() {
   const { tasks } = useTasks();
+  const { prepareTask } = useTask();
 
-  console.log(tasks);
+  function handleNewTask() {
+    prepareTask();
+  }
+
 
   return (
     <>
       <section>
-        <Button icon={PlusIcon} className="w-full">
+        <Button icon={PlusIcon} className="w-full" onClick={handleNewTask}>
           Nova tarefa
         </Button>
       </section>
